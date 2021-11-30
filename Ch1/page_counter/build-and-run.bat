575e07070ec3bcbfb9dd4c661c4ee6ce0fcbf807
Samir Gadkari <samir.gadkari@gmail.com>
2021-11-30 15:48:06 -0800

    Pass container name into bash script

--- Ch1/page_counter/build-and-run.bat
+++ Ch1/page_counter/build-and-run.bat
@@ -1,8 +1,14 @@
+#!/bin/bash
+
 # CGO_ENABLED flag is off. CGO is used if go code calls C code.
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/page-counter
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/$1
 
-ls -l bin/page-counter
+ls -l bin/$1
 
-docker build -t page-counter:v0.0.1 .
+docker build -t $1:v0.0.2 .
+docker tag $1:v0.0.2 samirgadkari/$1:v0.0.2
+docker login
+docker push samirgadkari/$1:v0.0.2
 
-docker run -p 8080:8080 -d page-counter:v0.0.1
+docker run --name db -d redis
+docker run --name $1 --link db:db -p 8080:8080 -d samirgadkari/$1:v0.0.2
