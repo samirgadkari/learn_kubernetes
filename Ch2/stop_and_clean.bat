8459a7c7341803efe55ffa2b72862715a56d0de1
Samir Gadkari <samir.gadkari@gmail.com>
2021-12-03 16:44:30 -0800

    Remove images using docker rmi, not docker rm

--- Ch2/stop_and_clean.bat
+++ Ch2/stop_and_clean.bat
@@ -10,7 +10,7 @@ kubectl delete deploy page-counter && kubectl delete service page-counter
 # echo 'stopping all containers'; docker stop $(docker ps -aq)
 # echo 'removing all containers'; docker rm $(docker ps -aq)
 # echo 'check that all containers are removed'; docker ps -a
-# echo 'removing all images'; docker rm image $(docker images | awk '{print $3}')
+# echo 'removing all images'; docker rmi image $(docker images | awk '{print $3}')
 # echo 'check that all images are removed'; docker images
 # exit
 # minikube stop
