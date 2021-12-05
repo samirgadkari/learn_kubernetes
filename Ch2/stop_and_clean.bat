7ff3a9df497eb98dcb80479ed7886aecd8d23162
Samir Gadkari <samir.gadkari@gmail.com>
2021-12-04 17:03:48 -0800

    Updated bash script to stop minikube

--- Ch2/stop_and_clean.bat
+++ Ch2/stop_and_clean.bat
@@ -1,16 +1,17 @@
 #!/bin/bash
-kubectl delete deploy page-counter && kubectl delete service page-counter
 
-# Since we don't have a way of passing a script to the minikube ssh
-# command, we cannot issue these commands inside the minikube shell.
-# We will have to visit this later.
-#
-# minikube ssh
-# sudo systemctl stop kubelet
-# echo 'stopping all containers'; docker stop $(docker ps -aq)
-# echo 'removing all containers'; docker rm $(docker ps -aq)
-# echo 'check that all containers are removed'; docker ps -a
-# echo 'removing all images'; docker rmi $(docker images | awk '{print $3}')
-# echo 'check that all images are removed'; docker images
-# exit
-# minikube stop
+# Use this script like this:
+#    Issue this command before using this script:
+#      kubectl delete deploy page-counter && kubectl delete service page-counter
+#    Then, run this command with this script
+#    minikube ssh 'bash -s' < this_script_file
+
+minikube ssh
+sudo systemctl stop kubelet
+echo 'stopping all containers'; docker stop $(docker ps -aq)
+echo 'removing all containers'; docker rm $(docker ps -aq)
+echo 'check that all containers are removed'; docker ps -a
+echo 'removing all images'; docker rmi $(docker images | awk '{print $3}')
+echo 'check that all images are removed'; docker images
+exit
+minikube stop
