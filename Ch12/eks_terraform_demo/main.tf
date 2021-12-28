40c5639ea145e253265dea684abca8666b9692d7
Samir Gadkari <samir.gadkari@gmail.com>
2021-12-28 14:07:17 -0800

    Added terraform example and updated .gitignore for terraform

---
+++ Ch12/eks_terraform_demo/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"      # Use us-west-2 region for aws provider
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "a123-test-bucket"
+  acl = "private"           # We will not allow public access to this bucket
+}
+
+# terraform init      # initialize terraform
+# terraform plan      # shows what it will provision
+# terraform apply     # will proppse changes, and will give you a chance to accept them,
+#  		      # before provisioning those changes.
+# terraform destroy   # Remove all resources allocated from files in this directory.
